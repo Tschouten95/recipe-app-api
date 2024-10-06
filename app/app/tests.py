2d4ebd2080a7643e4d3b51bae0a87e9feb5633ba
tim.schouten <tim.schouten@student.rocva.nl>
2024-10-06 21:01:13 +0200

    Refactor Dockerfile and docker-compose.yml
    - Added PostgreSQL client and dependencies for building
    - Removed temporary build dependencies
    - Added environment variables for database configuration in docker-compose.yml
    - Added volume for persistent database storage in docker-compose.yml
    
    Added calculator functions and tests
    
    - Added calculator functions for addition and subtraction in calc.py
    - Added tests for calculator functions in tests.py

---
+++ app/app/tests.py
@@ -0,0 +1,28 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """
+    Test the calc module
+    """
+
+    def test_add(self):
+        """
+        Test that two numbers are added together
+        """
+        res = calc.add(3, 8)
+
+        self.assertEqual(res, 11)
+    
+    def test_subtract(self):
+        """
+        Test that values are subtracted and returned
+        """
+        res = calc.subtract(5, 11)
+
+        self.assertEqual(res, 6)
